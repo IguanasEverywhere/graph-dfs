41799a5f4ee848530e07aedca0fda75058675176
Scott Schwab <scottschwab86@gmail.com>
2024-03-26 11:02:52 -0500

    Complete implementation

--- javascript/graph_dfs.js
+++ javascript/graph_dfs.js
@@ -1,27 +1,68 @@
-function isPath(graph, vertexA, vertexB) {
-  // type your code here
+function isPath(graph, vertexA, vertexB, visited = []) {
+  let adjs = graph[vertexA];
+
+  if (adjs.includes(vertexB)) {
+    return true;
+  }
+
+  while (adjs.length > 0) {
+    visited.push(vertexA);
+    if (visited.includes(adjs[0])) {
+      adjs.splice(0,1)
+      continue;
+    }
+    if (isPath(graph, adjs[0], vertexB, visited)) {
+      return true;
+    } else {
+      adjs.splice(0, 1);
+    }
+  }
+  return false;
 }
 
 if (require.main === module) {
-  // add your own tests in here
-  let graph = {
-    jan: ["john", "jambaby"],
+
+  const complexGraph = {
+    jan: ["john", "jambaby", "malala"],
     john: ["carl"],
     jambaby: [],
-    carl: ["jambaby"],
-    dave: []
+    carl: ["jambaby", "dave", "martin"],
+    dave: ["jan"],
+    mittens: [],
+    martin: ["mittens"],
+    malala: ["dave", "carl", "martin", "pirate"],
+    pirate: ["shiba", "inu"],
+    shiba: [],
+    inu: []
   };
 
-  console.log("Expecting: true");
-  console.log(isPath(graph, "jan", "carl"));
-
   console.log("");
+  console.log("Expecting: true");
+  console.log("");
+  console.log(isPath(complexGraph, "carl", "inu"))
 
-  console.log("Expecting: false");
-  console.log(isPath(graph, "jan", "dave"));
+
 }
 
 module.exports = isPath;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+
+// add your own tests in here
+// let graph = {
+//   jan: ["john", "jambaby"],
+//   john: ["carl"],
+//   jambaby: [],
+//   carl: ["jambaby"],
+//   dave: []
+// };
+
+// console.log("Expecting: true");
+// console.log(isPath(graph, "jan", "carl"));
+
+// console.log("");
+
+// console.log("Expecting: false");
+// console.log(isPath(graph, "jan", "dave"));
